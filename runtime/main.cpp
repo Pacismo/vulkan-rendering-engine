7a95d5660120efe31a9a3de50c11709d2ddfa0be
Francisco Santana <francisco.santana2002@gmail.com>
2024-08-31 22:05:15 -0500

    Better logging

--- runtime/main.cpp
+++ runtime/main.cpp
@@ -1,5 +1,41 @@
+#include <exceptions.hpp>
 #include <instance_manager.hpp>
+#include <spdlog/sinks/stdout_color_sinks.h>
 
-int main() {
-    engine::VulkanInstanceManager vim("runtime", {0, 1, 0});
+static void set_spdlog_global_settings()
+{
+    spdlog::set_pattern("[%D %T - %n: %^%l%$] %v");
+}
+
+static std::shared_ptr<spdlog::logger> make_logger()
+{
+    using stdout_color_sink = spdlog::sinks::stdout_color_sink_mt;
+    auto stdout_sink        = std::make_shared<stdout_color_sink>();
+
+    if constexpr (DEBUG_ASSERTIONS)
+        stdout_sink->set_level(spdlog::level::debug);
+    else
+        stdout_sink->set_level(spdlog::level::warn);
+
+    std::initializer_list<spdlog::sink_ptr> sinks = {stdout_sink};
+
+    auto logger = std::make_shared<spdlog::logger>("application", sinks);
+    spdlog::initialize_logger(logger);
+
+    return logger;
+}
+
+int main()
+{
+    set_spdlog_global_settings();
+    auto logger = make_logger();
+    spdlog::set_default_logger(logger);
+
+    try {
+        auto vim = engine::VulkanInstanceManager("runtime", {0, 1, 0});
+    } catch (engine::Exception &e) {
+        e.log();
+    }
+
+    logger->info("Done");
 }
