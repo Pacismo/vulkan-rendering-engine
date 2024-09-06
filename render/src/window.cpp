bf68f31726dd0e687257cb5318fa11ca711da8f4
Francisco Santana <francisco.santana2002@gmail.com>
2024-09-06 11:24:41 -0500

    Device selection moved to window

--- render/src/window.cpp
+++ render/src/window.cpp
@@ -17,65 +17,6 @@
 using std::string_view, std::span, std::vector, std::tuple, std::get, std::shared_ptr, std::stringstream, std::sort,
     std::move, std::swap;
 
-static std::string_view get_type(vk::PhysicalDeviceType t)
-{
-    switch (t) {
-    case vk::PhysicalDeviceType::eIntegratedGpu:
-        return "Integrated";
-    case vk::PhysicalDeviceType::eDiscreteGpu:
-        return "Discrete";
-    case vk::PhysicalDeviceType::eVirtualGpu:
-        return "Virtual";
-    default:
-        return "Other";
-    }
-}
-
-static void list_devices(shared_ptr<spdlog::logger>                                          logger,
-                         span<const tuple<vk::PhysicalDevice, vk::PhysicalDeviceProperties>> devices)
-{
-    stringstream ss = {};
-
-    for (auto &[device, properties] : devices)
-        ss << fmt::format("\n\t[{}] {} ({} MB)", get_type(properties.deviceType), properties.deviceName.data(),
-                          properties.limits.maxImageDimension2D);
-
-    logger->info("Found {} supported devices:{}", devices.size(), ss.str());
-}
-
-static vector<tuple<vk::PhysicalDevice, vk::PhysicalDeviceProperties>> get_properties(
-    span<const vk::PhysicalDevice> devices)
-{
-    vector<tuple<vk::PhysicalDevice, vk::PhysicalDeviceProperties>> pairs = {};
-    pairs.reserve(devices.size());
-
-    for (auto device : devices)
-        pairs.push_back({device, device.getProperties()});
-
-    return pairs;
-}
-
-static vk::PhysicalDevice select_physical_device(
-    span<const tuple<vk::PhysicalDevice, vk::PhysicalDeviceProperties>> devices)
-{
-    if (devices.size() == 0)
-        throw engine::Exception("No devices are available");
-
-    vk::PhysicalDevice selected_device;
-    uint64_t           selected_score = 0;
-
-    for (auto &[device, properties] : devices) {
-        uint64_t score = 0;
-        score += properties.limits.maxImageDimension2D;
-        score += (properties.deviceType == vk::PhysicalDeviceType::eDiscreteGpu) ? 100000 : 0;
-
-        if (score > selected_score)
-            selected_device = device, selected_score = score;
-    }
-
-    return selected_device;
-}
-
 namespace engine
 {
     Window::Window(string_view title, int32_t width, int32_t height, string_view application_name,
@@ -85,21 +26,14 @@ namespace engine
         if (!glfwInit())
             throw GlfwException("Failed to initialize GLFW");
 
-        auto instance_manager = VulkanInstanceManager::new_shared(application_name, application_version);
-
-        auto devices = get_properties(instance_manager->get_supported_rendering_devices());
-        list_devices(instance_manager->m_logger, devices);
-        auto device = select_physical_device(devices);
-
-        auto device_manager = RenderDeviceManager::new_shared(instance_manager, device);
-
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
         mp_window = glfwCreateWindow(width, height, title.data(), nullptr, nullptr);
         if (!mp_window)
             throw GlfwException("Failed to create a new window");
+        glfwSetWindowUserPointer(mp_window, this);
         m_logger->info("Created window");
 
-        m_render_manager = RenderManager::new_unique(device_manager, mp_window);
+        m_render_manager = RenderManager::new_unique(application_name, application_version, mp_window);
     }
 
     Window::Window(string_view title, int32_t width, int32_t height, const Window &other)
@@ -111,7 +45,7 @@ namespace engine
             throw GlfwException("Failed to create a new window");
         m_logger->info("Created window");
 
-        m_render_manager = RenderManager::new_unique(other.m_render_manager->m_device_manager, mp_window);
+        m_render_manager = RenderManager::new_unique(other.m_render_manager, mp_window);
     }
 
     void Window::show()
@@ -134,7 +68,10 @@ namespace engine
         while (!glfwWindowShouldClose(mp_window)) {
             glfwPollEvents();
             process();
+            m_render_manager->render_frame();
         }
+
+        m_render_manager->wait_idle();
     }
 
     Window::~Window()
