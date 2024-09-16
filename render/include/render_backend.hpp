efe95ba2e4374a0918b0a9bf102bba26761c90b1
Francisco Santana <francisco.santana2002@gmail.com>
2024-09-16 16:18:34 -0500

    Projection/view

--- render/include/render_backend.hpp
+++ render/include/render_backend.hpp
@@ -13,7 +13,8 @@ namespace engine
     class RenderBackend
     {
       public:
-        using DrawHandler = std::function<void(struct DrawingContext &)>;
+        virtual void update_projection(float fov)         = 0;
+        virtual void set_view(const glm::mat4 &transform) = 0;
 
         virtual std::shared_ptr<Object> load(std::span<primitives::GouraudVertex> vertices,
                                              std::span<uint32_t>                  indices) = 0;
