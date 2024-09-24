06c314d1e2d0eecfc58faca71b494a965d47e86b
Francisco Santana <francisco.santana2002@gmail.com>
2024-09-23 21:26:03 -0500

    Single-time command buffer for device (independent of render manager)

--- engine/src/backend/device_manager.cpp
+++ engine/src/backend/device_manager.cpp
@@ -15,6 +15,37 @@ static vk::PhysicalDeviceFeatures REQUIRED_DEVICE_FEATURES   = {};
 
 namespace engine
 {
+    SingleTimeCommandBuffer::SingleTimeCommandBuffer(RenderDeviceManager *manager, vk::Queue queue,
+                                                     vk::CommandBuffer buffer)
+        : p_manager(manager)
+        , queue(queue)
+        , buffer(buffer)
+    {
+        buffer.begin(vk::CommandBufferBeginInfo {.flags = vk::CommandBufferUsageFlagBits::eOneTimeSubmit});
+    }
+
+    SingleTimeCommandBuffer::~SingleTimeCommandBuffer()
+    {
+        if (p_manager)
+            p_manager->device.freeCommandBuffers(p_manager->command_pool, buffer);
+    }
+
+    void SingleTimeCommandBuffer::submit()
+    {
+        buffer.end();
+
+        queue.submit(vk::SubmitInfo {
+            .commandBufferCount = 1,
+            .pCommandBuffers    = &buffer,
+        });
+        queue.waitIdle();
+        p_manager->device.freeCommandBuffers(p_manager->command_pool, buffer);
+
+        p_manager = nullptr;
+        queue     = nullptr;
+        buffer    = nullptr;
+    }
+
     static tuple<optional<uint32_t>, optional<uint32_t>> get_gp_queue_indices(vk::Instance       instance,
                                                                               vk::PhysicalDevice device)
     {
@@ -46,6 +77,16 @@ namespace engine
       public:
         void operator()(RenderDeviceManager *ptr) { delete ptr; }
     };
+
+    SingleTimeCommandBuffer engine::RenderDeviceManager::single_time_command()
+    {
+        return SingleTimeCommandBuffer(this, graphics_queue.handle,
+                                       device.allocateCommandBuffers(vk::CommandBufferAllocateInfo {
+                                           .commandPool        = command_pool,
+                                           .level              = vk::CommandBufferLevel::ePrimary,
+                                           .commandBufferCount = 1,
+                                       })[0]);
+    }
 
     RenderDeviceManager::RenderDeviceManager(SharedInstanceManager instance_manager, vk::PhysicalDevice physical_device)
         : logger(instance_manager->logger)
@@ -89,20 +130,18 @@ namespace engine
                 .pQueuePriorities = &queue_priority,
             });
 
-        {
-            vk::DeviceCreateInfo dci = {
-                .queueCreateInfoCount    = (uint32_t)queue_create_infos.size(),
-                .pQueueCreateInfos       = queue_create_infos.data(),
-                .enabledLayerCount       = 0,
-                .ppEnabledLayerNames     = nullptr,
-                .enabledExtensionCount   = (uint32_t)device_extensions.size(),
-                .ppEnabledExtensionNames = device_extensions.data(),
-                .pEnabledFeatures        = &REQUIRED_DEVICE_FEATURES,
-            };
+        vk::DeviceCreateInfo dci = {
+            .queueCreateInfoCount    = (uint32_t)queue_create_infos.size(),
+            .pQueueCreateInfos       = queue_create_infos.data(),
+            .enabledLayerCount       = 0,
+            .ppEnabledLayerNames     = nullptr,
+            .enabledExtensionCount   = (uint32_t)device_extensions.size(),
+            .ppEnabledExtensionNames = device_extensions.data(),
+            .pEnabledFeatures        = &REQUIRED_DEVICE_FEATURES,
+        };
 
-            device = physical_device.createDevice(dci);
-            logger->info("Created device");
-        }
+        device = physical_device.createDevice(dci);
+        logger->info("Created device");
 
         graphics_queue = Queue {
             .index  = graphics_queue_index.value(),
@@ -113,6 +152,13 @@ namespace engine
             .handle = device.getQueue(present_queue_index.value(), 0),
         };
 
+        vk::CommandPoolCreateInfo cpi = {
+            .flags            = vk::CommandPoolCreateFlagBits::eResetCommandBuffer,
+            .queueFamilyIndex = graphics_queue.index,
+        };
+
+        command_pool = device.createCommandPool(cpi);
+
         logger->info("Selected queue family {} for graphics queue ({:8X})", graphics_queue.index,
                      (size_t)(VkQueue)graphics_queue.handle);
         logger->info("Selected queue family {} for present queue ({:8X})", present_queue.index,
@@ -121,8 +167,8 @@ namespace engine
 
     RenderDeviceManager::~RenderDeviceManager()
     {
-        if (device)
-            device.destroy();
+        device.destroyCommandPool(command_pool);
+        device.destroy();
         logger->info("Destroyed device");
 
         graphics_queue  = {};
@@ -147,5 +193,22 @@ namespace engine
                                                                 vk::PhysicalDevice    physical_device)
     {
         return {new RenderDeviceManager(instance_manager, physical_device), Deleter()};
+    }
+
+    vk::Format engine::RenderDeviceManager::find_supported_format(std::span<const vk::Format> formats,
+                                                                  vk::ImageTiling             tiling,
+                                                                  vk::FormatFeatureFlags      features) const
+    {
+        for (auto format : formats) {
+            vk::FormatProperties properties = physical_device.getFormatProperties(format);
+
+            if ((tiling == vk::ImageTiling::eLinear) && (properties.linearTilingFeatures & features))
+                return format;
+            else if ((tiling == vk::ImageTiling::eOptimal) && (properties.optimalTilingFeatures & features))
+                return format;
+        }
+
+        throw engine::VulkanException((uint32_t)vk::Result::eErrorFormatNotSupported,
+                                      "Failed to find a supported format");
     }
 } // namespace engine
