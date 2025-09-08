c76b680295e988a1bbb9d8b0505890736a087971
Francisco Santana <franciscosp-dev@outlook.com>
2025-09-08 12:38:25 -0400

    Added debug note

--- runtime/RuntimeInfo.cpp
+++ runtime/RuntimeInfo.cpp
@@ -12,4 +12,7 @@ void RuntimeInfo::populate(ImGuiViewport *viewport)
 {
     ImGui::Text("Engine: %s", ENGINE_NAME);
     ImGui::Text("Version: %u.%u.%u", ENGINE_VERSION_MAJOR, ENGINE_VERSION_MINOR, ENGINE_VERSION_PATCH);
+
+    if (DEBUG_ASSERTIONS)
+        ImGui::Text("Debugging enabled");
 }
