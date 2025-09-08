fb82496f91928b2693437732e58f57fd9990dd46
Francisco Santana <franciscosp-dev@outlook.com>
2025-09-08 12:30:46 -0400

    Runtime information

--- runtime/RuntimeInfo.cpp
+++ runtime/RuntimeInfo.cpp
@@ -1,6 +1,6 @@
 #include "RuntimeInfo.hpp"
-
-using namespace engine::reflection;
+#include <fmt/format.h>
+#include <imgui_stdlib.h>
 
 RuntimeInfo::RuntimeInfo()
     : Applet("Runtime Information", false, true)
@@ -10,5 +10,6 @@ RuntimeInfo::~RuntimeInfo() { }
 
 void RuntimeInfo::populate(ImGuiViewport *viewport)
 {
-
+    ImGui::Text("Engine: %s", ENGINE_NAME);
+    ImGui::Text("Version: %u.%u.%u", ENGINE_VERSION_MAJOR, ENGINE_VERSION_MINOR, ENGINE_VERSION_PATCH);
 }
